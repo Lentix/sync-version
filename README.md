54fbd4be8e615ad8a37c9602736584853f37600f
Koridore <jamesrichardpryor@gmail.com>
2023-01-26 10:21:38 -0500

    Initial Commit

--- README.md
+++ README.md
@@ -1,1 +1,55 @@
-# sync-version
+sync-version
+============
+A command line tool to sync version number with package.json.
+
+Install
+-------
+```
+npm install -D sync-version
+```
+
+Usage
+-----
+```
+Usage:
+  sync-version [--list] <files>...
+  
+Options:
+  <files>    Sync version number with package.json inside <file>.
+  -l --list  List current version.
+```
+
+Use it with package.json script:
+
+```json
+"scripts": {
+	"version": "sync-version bower.json && git add ."
+}
+```
+
+How does it work?
+-----------------
+
+The script will try finding the version number with
+
+```
+/\bversion["']?:\s*["']?([^\s'"]+)/
+/^\/\/ @version\s+(\S+)/
+```
+
+and update it.
+
+Changelog
+---------
+* 1.0.1 (May 16, 2017)
+	- Fix: crlf breaks the executable on linux. [#1](https://github.com/eight04/sync-version/issues/1)
+* 1.0.0 (Jan 17, 2017)
+	- Change RegExp, shouldn't match "whatever_**version**".
+	- Display package name on start.
+	- Drop app-root-path. Find package.json along the ascension folders from cwd.
+* 0.2.0 (Nov 7, 2016)
+	- Support userscript style `// @version 0.1.0`.
+* 0.1.1 (Apr 17, 2016)
+	- Use app-root-path.
+* 0.1.0 (Apr 16, 2016)
+	- First release.
---
+++ sync-version.js
@@ -0,0 +1,103 @@
+#!/usr/bin/env node
+
+var doc = `sync-version
+
+Usage:
+  sync-version [--list] [<files>...]
+  
+Options:
+  <files>    Sync version number with package.json inside <file>.
+  -l --list  List current version.`,
+
+	fs = require("fs"),
+	docopt = require("docopt"),
+	path = require("path"),
+	pkg = getPackage(),
+	
+	args = docopt.docopt(doc, {version: "1.0.1"}),
+	
+	res = [
+		/\bversion["']?:\s*["']?([^\s'"]+)/,
+		/^\/\/ @version\s+(\S+)/m
+	];
+	
+if (!pkg.version) {
+	throw "cannot find version property in package.json";
+}
+
+console.log("Under " + pkg.name);
+console.log("package.json\t" + pkg.version);
+
+var i, files = args["<files>"];
+for (i = 0; i < files.length; i++) {
+	if (args["--list"]) {	
+		logVersion(files[i]);
+	} else {
+		syncVersion(files[i]);
+	}
+}
+
+function getPackage() {
+	var searchPath = process.cwd(),
+		pkg, pkgPath, nextSearchPath;
+	
+	do {
+		try {
+			pkgPath = path.join(searchPath, "package.json");
+			pkg = fs.readFileSync(pkgPath);
+		} catch (err) {
+			// pass
+		}
+		nextSearchPath = path.dirname(searchPath);
+		if (searchPath == nextSearchPath) {
+			throw "Can't find package.json";
+		}
+		searchPath = nextSearchPath;
+	} while (pkg == null);
+
+	return JSON.parse(pkg);
+}
+	
+function matchVersion(data) {
+	var i, match;
+	for (i = 0; i < res.length; i++) {
+		match = data.match(res[i]);
+		if (match) return match;
+	}
+}
+
+function logVersion(file) {
+	fs.readFile(file, "utf8", function(err, data){
+		if (err) {
+			throw err;
+		}
+		var match = matchVersion(data);
+		if (!match) {
+			throw "cannot find version property in " + file;
+		}
+		console.log(file + "\t" + match[1]);
+	});
+}
+
+function syncVersion(file) {
+	fs.readFile(file, "utf8", function(err, data) {
+		if (err) {
+			throw err;
+		}
+		var match = matchVersion(data);
+		if (!match) {
+			throw "cannot find version property in " + file;
+		}
+		if (match[1] == pkg.version) {
+			console.log(file + "\t" + match[1] + " --> " + pkg.version + " no change");
+			return;
+		}
+		data = data.substring(0, match.index + match[0].length - match[1].length) + pkg.version + data.substring(match.index + match[0].length);
+		fs.writeFile(file, data, "utf8", function (err) {
+			if (err) {
+				throw err;
+			}
+			console.log(file + "\t" + match[1] + " --> " + pkg.version);
+		});
+	});
+}
